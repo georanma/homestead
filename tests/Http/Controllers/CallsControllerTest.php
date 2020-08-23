defa46aa37eb80598ee4f9054d61b243f3f68499
George Birnbaum <gb@glamorise.com>
2020-08-23 14:41:42 -0400

    clean up

---
+++ tests/Http/Controllers/CallsControllerTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Unit\Http\Controllers;
+
+use App\Http\Controllers\CallsController;
+use PHPUnit\Framework\TestCase;
+
+class CallsControllerTest extends TestCase
+{
+
+
+
+}
